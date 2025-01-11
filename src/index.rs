c3352f90f97189d68a78985bbfcd4f85643b3eae
Yabir Benchakhtir <yabirg@protonmail.com>
2025-01-11 16:45:30 +0100

    Search in the index for the appearences of addresses

--- src/index.rs
+++ src/index.rs
@@ -1,14 +1,17 @@
-use std::{fs::File, io::Read, path::PathBuf};
+use std::io::{self, Read, Seek, SeekFrom};
+use std::{fs::File, path::PathBuf};
 
 use crate::address::Address;
 
-const HEADER_WIDTH: u64 = 44;
+const HEADER_WIDTH: usize = 44;
+const ADDRESS_RECORD_WIDTH: usize = 28;
+const APP_RECORD_WIDTH: usize = 8;
 
-struct HeaderRecord {
-    magic: u32,
-    version: [u8; 32],
-    num_addresses: u32,
-    num_appearances: u32,
+pub struct HeaderRecord {
+    pub magic: u32,
+    pub version: [u8; 32],
+    pub num_addresses: u32,
+    pub num_appearances: u32,
 }
 
 impl HeaderRecord {
@@ -33,20 +36,114 @@ impl HeaderRecord {
     }
 }
 
-struct AddressRecord {
-    address: Address,
-    offset: u32,
-    count: u32,
+pub struct AddressRecord {
+    pub address: Address,
+    pub offset: u32,
+    pub count: u32,
 }
 
-struct AppearanceRecord {
-    block: u32,
-    tx_index: u32,
+#[derive(Debug)]
+pub struct AppearanceRecord {
+    pub block: u32,
+    pub tx_index: u32,
 }
 
 pub struct Index {
-    file: File,
-    header: HeaderRecord,
-    address_table_start: u64,
-    app_table_start: u64,
+    pub file: File,
+    pub header: HeaderRecord,
+    pub address_table_start: u64,
+    pub app_table_start: u64,
+}
+
+impl Index {
+    fn binary_search(&mut self, target_address: &Address) -> io::Result<usize> {
+        let address_count = self.header.num_addresses as usize;
+
+        // we don't know the size of the slice at comp time so we need to use
+        // a vec. binary_search_by is not implemented for ranges.
+        let pos = (0..address_count)
+            .collect::<Vec<usize>>()
+            .binary_search_by(|&pos| {
+                // Handle edge cases
+                if pos == 0 {
+                    return std::cmp::Ordering::Less;
+                }
+                if pos == address_count {
+                    return std::cmp::Ordering::Equal;
+                }
+
+                // Calculate read position
+                let read_location = (HEADER_WIDTH + pos * ADDRESS_RECORD_WIDTH) as u64;
+                if let Err(e) = self.file.seek(SeekFrom::Start(read_location)) {
+                    eprintln!("Seek error: {}", e);
+                    return std::cmp::Ordering::Less;
+                }
+
+                // Read address record
+                let mut addr_record = [0; 32];
+                match self.file.read_exact(&mut addr_record) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        eprintln!("Read error: {}", e);
+                        return std::cmp::Ordering::Less;
+                    }
+                }
+
+                // Compare addresses
+                addr_record.as_slice().cmp(target_address)
+            });
+
+        match pos {
+            Ok(index) => Ok(index),
+            Err(index) => Ok(index), // Return insertion point if not found
+        }
+    }
+
+    pub fn search_for_address_record(&mut self, address: &Address) -> AddressRecord {
+        let pos = self
+            .binary_search(address)
+            .expect("Address not found in index");
+        let read_location = HEADER_WIDTH + pos * ADDRESS_RECORD_WIDTH;
+        self.file
+            .seek(SeekFrom::Start(read_location as u64))
+            .unwrap();
+
+        let mut address_buf = [0; 20];
+        let mut offset_buf = [0; 4];
+        let mut count_buf = [0; 4];
+
+        self.file.read_exact(&mut address_buf).unwrap();
+        self.file.read_exact(&mut offset_buf).unwrap();
+        self.file.read_exact(&mut count_buf).unwrap();
+
+        AddressRecord {
+            address: address_buf,
+            offset: u32::from_le_bytes(offset_buf),
+            count: u32::from_le_bytes(count_buf),
+        }
+    }
+
+    pub fn read_apparences(&mut self, address: &Address) -> Vec<AppearanceRecord> {
+        let mut results: Vec<AppearanceRecord> = Vec::new();
+        let record = self.search_for_address_record(address);
+
+        let read_location = (HEADER_WIDTH
+            + ADDRESS_RECORD_WIDTH * self.header.num_addresses as usize
+            + APP_RECORD_WIDTH * record.offset as usize) as u64;
+
+        self.file.seek(SeekFrom::Start(read_location)).unwrap();
+
+        let mut block_buf = [0; 4];
+        let mut index_buf = [0; 4];
+
+        for _ in (0..record.count) {
+            self.file.read_exact(&mut block_buf).unwrap();
+            self.file.read_exact(&mut index_buf).unwrap();
+            results.push(AppearanceRecord {
+                block: u32::from_le_bytes(block_buf),
+                tx_index: u32::from_le_bytes(index_buf),
+            });
+        }
+        results
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
+use std::fs::File;
 use std::path::PathBuf;
 
 use missuri::address::address_from_string;
 use missuri::bloom;
+use missuri::index;
 
 fn main() -> std::io::Result<()> {
     // Open the file 4945
-    let path =
-        PathBuf::from("/home/yabirgb/Downloads/QmQfn7HkkyjiipBMYvnoQExp7G26NVv17a1pJZyPGpVuf6");
-    let bloom = bloom::Bloom::read_from_file(path).unwrap();
-    // E6c968B7d1b0f5FB6810036d6922aF3372Ffef11
-    // https://etherscan.io/tx/0x99e071789fd914f243fd4ba1fa88852f0429a50eae32a301c1fb4ff4adac0feb
+    // let path =
+    //     PathBuf::from("/home/yabirgb/Downloads/QmQfn7HkkyjiipBMYvnoQExp7G26NVv17a1pJZyPGpVuf6");
+    // let bloom = bloom::Bloom::read_from_file(path).unwrap();
+    // // E6c968B7d1b0f5FB6810036d6922aF3372Ffef11
+    // // https://etherscan.io/tx/0x99e071789fd914f243fd4ba1fa88852f0429a50eae32a301c1fb4ff4adac0feb
     let raw_addr = "2228e5704B637131A3798A186CAF18366c146f74".to_string();
-    // non existing one
-    // let raw_addr = "1C5ABce0cAf0f92CF8b083c1b3e8bdda9AD24249".to_string();
+    // // non existing one
+    // // let raw_addr = "1C5ABce0cAf0f92CF8b083c1b3e8bdda9AD24249".to_string();
     let addr = address_from_string(&raw_addr);
-    println!(
-        "is 2228e5704B637131A3798A186CAF18366c146f74 in the bloom: {}",
-        bloom.address_is_member(addr)
-    );
+    // println!(
+    //     "is 2228e5704B637131A3798A186CAF18366c146f74 in the bloom: {}",
+    //     bloom.address_is_member(addr)
+    // );
     //println!("{:?}", &bloom.bloom_filters[4].bytes);
+    let index_path = PathBuf::from("./data/QmePxCpxtCQSDVcGTbQaXNARQjs1Us2WH6tXvixQEqZjCG");
+    let index_file = File::open(index_path.clone()).unwrap();
+    let header = index::HeaderRecord::read_from_file(index_path);
+    println!("{}", header.num_addresses);
+    let mut index = index::Index {
+        file: index_file,
+        header: header,
+        address_table_start: 0,
+        app_table_start: 0,
+    };
+
+    for app in index.read_apparences(&addr) {
+        println!(
+            "{} - {}; 0x{:x} - 0x{:x}",
+            app.block, app.tx_index, app.block, app.tx_index
+        );
+    }
+
     Ok(())
 }
